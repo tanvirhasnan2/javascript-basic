8d409acf39663c878bb34139a7800344c0273942
Tanvir Hasnan <tanvirhasnan2@gmail.com>
2021-10-21 19:08:41 +0600

    Practice Iterator

--- webpack/src/scripts/index.js
+++ webpack/src/scripts/index.js
@@ -8,19 +8,55 @@ import '../styles/index.scss'
 
 // console.log(s)
 
-var name = `Tanvri Hasnan`
-var age = 28
+// var name = `Tanvri Hasnan`
+// var age = 28
 
-console.log(`My name is: ${name} and I am ${age} years old. I am ${age < 18 ? 'not ' : ''}adult`);
-
-
-var sqr = (n=1) => n*n
-console.log(sqr(5));
+// console.log(`My name is: ${name} and I am ${age} years old. I am ${age < 18 ? 'not ' : ''}adult`);
 
 
-var greet = (name = 'Tanvir', msg) => {
-    console.log(name.length)
-    console.log(`${msg}! ${name}`)
-}
+// var sqr = (n=1) => n*n
+// console.log(sqr(5));
 
-greet('hello')
+
+// var greet = (name = 'Tanvir', msg) => {
+//     console.log(name.length)
+//     console.log(`${msg}! ${name}`)
+// }
+
+// greet('hello')
+
+
+
+
+//Iterator
+// let obj = {
+//     start: 1,
+//     end: 5,
+//     [Symbol.iterator]: function(){
+//         let currentValue = this.start
+//         const self = this
+//         return {
+//             next() {
+//                 return {
+//                     done: currentValue > self.end,
+//                     value: self.end ? undefined : currentValue++
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// for (let v of obj){
+//     console.log(v);
+// }
+
+// let iterate = obj[Symbol.iterator]()
+// console.log(iterate.next);
+// console.log(iterate.next);
+// console.log(iterate.next);
+// console.log(iterate.next);
+// console.log(iterate.next);
+// console.log(iterate.next);
+// console.log(iterate.next);
+
+
